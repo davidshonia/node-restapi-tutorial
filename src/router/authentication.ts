5c206879a7e46f8fba45cb9edd91ef970798aa54
David <dupecube@gmail.com>
2023-05-21 01:27:20 +0400

    added login endpoint on authentication route

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,7 +1,8 @@
 import express from "express";
 
-import {register} from "../controller/authentication";
+import {register, login} from "../controller/authentication";
 
 export default (router: express.Router) => {
     router.post('/auth/register', register)
+    router.post('/auth/login', login)
 }
