c177bcf04174ac278c389365a618cbb8c9514de4
David <dupecube@gmail.com>
2023-05-21 01:25:07 +0400

    ended login endpoint: added comments on register and login functions

--- src/controller/authentication.ts
+++ src/controller/authentication.ts
@@ -12,17 +12,26 @@ export const login = async (req: express.Request, res: express.Response) => {
 
         const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
 
+        // User not found
         if (!user) {
             return res.sendStatus(400);
         }
 
+        // Check if user password matches with hash
         const expectedHash = authentication(user.authentication.salt, password)
-
         if (user.authentication.password != expectedHash) {
             return res.sendStatus(403);
         }
 
+        // Update User Session Token In Database
         const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString())
+
+        // Save User After Updating Token
+        await user.save()
+
+        // Return Cookie
+        res.cookie('AUTH-LOGIN', user.authentication.sessionToken, {domain:'localhost'})
 
         return res.status(200).json(user).end()
     } catch (err) {
@@ -34,20 +43,22 @@ export const login = async (req: express.Request, res: express.Response) => {
 
 export const register = async (req: express.Request, res: express.Response) => {
     try {
+        // Request body fields
         const {email, password, username} = req.body
 
+        // Required Fields
         if (!email || !password || !username) {
             return res.sendStatus(400)
         }
 
+        // Get user if exists else
         const existingUser = await getUserByEmail(email)
-
         if (existingUser) {
             return res.sendStatus(400)
         }
 
+        // Create user if not exists
         const salt = random()
-
         const user = await createUser({
             email,
             username,
